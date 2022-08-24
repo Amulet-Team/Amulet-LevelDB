1a7a5f17fd185f655c8d813dbcca4125327ba555
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-08-24 15:23:06 +0100

    Updated the build system

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 import sys
+import os
+
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import versioneer
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "build_tools"))
+
+import include_binary
+
+cmdclass = versioneer.get_cmdclass()
+include_binary.register(cmdclass)
 
 if sys.platform == "win32":
     define_macros = [
@@ -24,75 +33,24 @@ elif sys.platform == "darwin":
 else:
     raise Exception("Unsupported platform")
 
+library_dirs = [os.path.join("bin", include_binary.get_bin_dir())]
+
 
 setup(
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    cmdclass=cmdclass,
     ext_modules=cythonize(
         Extension(
             name="leveldb._leveldb",
-            sources=[
-                # cython
-                "./leveldb/_leveldb.pyx",
-
-                # leveldb
-                "./leveldb_mcpe/db/builder.cc",
-                "./leveldb_mcpe/db/c.cc",
-                "./leveldb_mcpe/db/db_impl.cc",
-                "./leveldb_mcpe/db/db_iter.cc",
-                "./leveldb_mcpe/db/dbformat.cc",
-                "./leveldb_mcpe/db/filename.cc",
-                "./leveldb_mcpe/db/log_reader.cc",
-                "./leveldb_mcpe/db/log_writer.cc",
-                "./leveldb_mcpe/db/memtable.cc",
-                "./leveldb_mcpe/db/repair.cc",
-                "./leveldb_mcpe/db/table_cache.cc",
-                "./leveldb_mcpe/db/version_edit.cc",
-                "./leveldb_mcpe/db/version_set.cc",
-                "./leveldb_mcpe/db/write_batch.cc",
-                "./leveldb_mcpe/db/zlib_compressor.cc",
-                "./leveldb_mcpe/db/zstd_compressor.cc",
-
-                "./leveldb_mcpe/table/block.cc",
-                "./leveldb_mcpe/table/block_builder.cc",
-                "./leveldb_mcpe/table/filter_block.cc",
-                "./leveldb_mcpe/table/format.cc",
-                "./leveldb_mcpe/table/iterator.cc",
-                "./leveldb_mcpe/table/merger.cc",
-                "./leveldb_mcpe/table/table.cc",
-                "./leveldb_mcpe/table/table_builder.cc",
-                "./leveldb_mcpe/table/two_level_iterator.cc",
-
-                "./leveldb_mcpe/util/arena.cc",
-                "./leveldb_mcpe/util/bloom.cc",
-                "./leveldb_mcpe/util/cache.cc",
-                "./leveldb_mcpe/util/coding.cc",
-                "./leveldb_mcpe/util/comparator.cc",
-                "./leveldb_mcpe/util/crc32c.cc",
-                "./leveldb_mcpe/util/env.cc",
-                # "./leveldb_mcpe/util/env_boost.cc",
-                "./leveldb_mcpe/util/env_posix.cc",
-                "./leveldb_mcpe/util/env_win.cc",
-                "./leveldb_mcpe/util/filter_policy.cc",
-                "./leveldb_mcpe/util/hash.cc",
-                "./leveldb_mcpe/util/histogram.cc",
-                "./leveldb_mcpe/util/logging.cc",
-                "./leveldb_mcpe/util/options.cc",
-                "./leveldb_mcpe/util/status.cc",
-                "./leveldb_mcpe/util/win_logger.cc",
-
-                "./leveldb_mcpe/port/port_posix.cc",
-                "./leveldb_mcpe/port/port_posix_sse.cc",
-                "./leveldb_mcpe/port/port_win.cc",
-            ],
+            sources=["./leveldb/_leveldb.pyx"],
             include_dirs=[
-                "zlib",
-                "leveldb_mcpe/include",
                 "leveldb_mcpe",
+                "leveldb_mcpe/include",
             ],
             language="c++",
-            # libraries=["stdc++"],
-            define_macros=define_macros
+            library_dirs=library_dirs,
+            libraries=["leveldb"],
+            define_macros=define_macros,
         ),
         language_level=3,
     ),

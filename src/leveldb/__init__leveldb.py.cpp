9692d7a15fe44fc35fc1a208d0c3f992438d5c45
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-11-29 11:08:16 +0000

    Added compiler metadata
    The libraries must be compiled with the same version of the compiler and pybind11.
    If they are compiled with different versions bad things happen.
    Adding this data at compile time allows dependent libraries to validate compatibility.

--- src/leveldb/__init__leveldb.py.cpp
+++ src/leveldb/__init__leveldb.py.cpp
@@ -295,6 +295,12 @@ public:
 
 static void init_module(py::module m)
 {
+    py::dict compiler_config;
+    compiler_config["pybind11_version"] = PYBIND11_VERSION;
+    compiler_config["compiler_id"] = COMPILER_ID;
+    compiler_config["compiler_version"] = COMPILER_VERSION;
+    m.attr("compiler_config") = compiler_config;
+
     py::register_local_exception<LevelDBException>(m, "LevelDBException");
     py::register_local_exception<LevelDBEncrypted>(m, "LevelDBEncrypted");
 

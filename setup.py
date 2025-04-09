cc4b7cfebca09da8311555ff8e33d6137a4869ab
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-04-09 15:30:51 +0100

    Improve build tooling

--- setup.py
+++ setup.py
@@ -2,21 +2,30 @@ import os
 import subprocess
 import sys
 from pathlib import Path
-import pybind11
-import shutil
 
 from setuptools import setup, Extension, Command
 from setuptools.command.build_ext import build_ext
 
 import versioneer
-import pybind11_extensions
 
+dependencies = [
+    "amulet-compiler-target==1.0",
+]
+setup_args = {}
 
-# https://github.com/pybind/cmake_example/blob/master/setup.py
-class CMakeExtension(Extension):
-    def __init__(self, name: str, sourcedir: str = "") -> None:
-        super().__init__(name, sources=[])
-        self.sourcedir = os.fspath(Path(sourcedir).resolve())
+try:
+    import amulet_compiler_version
+except ImportError:
+    dependencies.append("amulet-compiler-version==1.3.0")
+else:
+    dependencies.append(
+        f"amulet-compiler-version=={amulet_compiler_version.__version__}"
+    )
+    setup_args["options"] = {
+        "bdist_wheel": {
+            "build_number": f"1.{amulet_compiler_version.compiler_id}.{amulet_compiler_version.compiler_version}"
+        }
+    }
 
 
 cmdclass: dict[str, type[Command]] = versioneer.get_cmdclass()
@@ -24,6 +33,9 @@ cmdclass: dict[str, type[Command]] = versioneer.get_cmdclass()
 
 class CMakeBuild(cmdclass.get("build_ext", build_ext)):
     def build_extension(self, ext):
+        import pybind11
+        import pybind11_extensions
+
         ext_fullpath = Path.cwd() / self.get_ext_fullpath("")
         src_dir = ext_fullpath.parent.resolve()
 
@@ -68,5 +80,7 @@ cmdclass["build_ext"] = CMakeBuild
 setup(
     version=versioneer.get_version(),
     cmdclass=cmdclass,
-    ext_modules=[CMakeExtension("leveldb._leveldb")],
+    ext_modules=[Extension("leveldb._leveldb", [])],
+    install_requires=dependencies,
+    **setup_args,
 )

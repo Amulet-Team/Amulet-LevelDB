07ee908b8f287bf78da22fb119fa884976f6460f
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-06-09 16:44:02 +0100

    Fix cmake config (#33)

--- src/amulet/leveldb/leveldb_mcpeConfig.cmake
+++ src/amulet/leveldb/leveldb_mcpeConfig.cmake
@@ -6,16 +6,17 @@ if (NOT TARGET leveldb_mcpe)
     find_library(leveldb_mcpe_LIBRARY NAMES leveldb_mcpe PATHS "${CMAKE_CURRENT_LIST_DIR}")
     message(STATUS "leveldb_mcpe_LIBRARY: ${leveldb_mcpe_LIBRARY}")
 
-    add_library(leveldb_mcpe SHARED IMPORTED)
-    set_target_properties(leveldb_mcpe PROPERTIES
+    add_library(leveldb_mcpe_bin SHARED IMPORTED)
+    set_target_properties(leveldb_mcpe_bin PROPERTIES
         IMPORTED_IMPLIB "${leveldb_mcpe_LIBRARY}"
     )
 
-    add_library(leveldb_mcpe_interface INTERFACE)
-    target_include_directories(leveldb_mcpe_interface INTERFACE ${leveldb_mcpe_INCLUDE_DIR})
+    add_library(leveldb_mcpe INTERFACE)
+    target_link_libraries(leveldb_mcpe INTERFACE leveldb_mcpe_bin)
+    target_include_directories(leveldb_mcpe INTERFACE ${leveldb_mcpe_INCLUDE_DIR})
     if (WIN32)
-        target_compile_definitions(leveldb_mcpe_interface INTERFACE "DLLX=__declspec(dllimport)")
+        target_compile_definitions(leveldb_mcpe INTERFACE "DLLX=__declspec(dllimport)")
     else()
-        target_compile_definitions(leveldb_mcpe_interface INTERFACE "DLLX=")
+        target_compile_definitions(leveldb_mcpe INTERFACE "DLLX=")
     endif()
 endif()

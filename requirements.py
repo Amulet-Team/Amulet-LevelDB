f5c303d95a201354ec8cbc9e55b40f4319665b61
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-07-10 10:30:34 +0100

    Improve build tools

--- requirements.py
+++ requirements.py
@@ -4,7 +4,7 @@ import get_compiler
 
 AMULET_COMPILER_TARGET_REQUIREMENT = "==2.0"
 
-PYBIND11_REQUIREMENT = "==3.0.1"
+PYBIND11_REQUIREMENT = "==3.0.4"
 AMULET_PYBIND11_EXTENSIONS_REQUIREMENT = "~=1.2.0.0a2"
 
 if os.environ.get("AMULET_PYBIND11_EXTENSIONS_REQUIREMENT", None):
@@ -22,9 +22,6 @@ def get_specifier_set(version_str: str) -> str:
     return f"~={version.major}.{version.minor}.{version.micro}.0{''.join(map(str, version.pre or ()))}"
 
 
-AMULET_COMPILER_VERSION_REQUIREMENT = get_compiler.main()
-
-
 try:
     import amulet.pybind11_extensions
 except ImportError:
@@ -37,16 +34,16 @@ else:
 
 def get_build_dependencies() -> list:
     return [
-        f"amulet-compiler-version{AMULET_COMPILER_VERSION_REQUIREMENT}",
+        f"amulet-compiler-version{get_compiler.main()}",
         f"pybind11{PYBIND11_REQUIREMENT}",
         f"amulet-pybind11-extensions{AMULET_PYBIND11_EXTENSIONS_REQUIREMENT}",
     ] * (not os.environ.get("AMULET_SKIP_COMPILE", None))
 
 
-def get_runtime_dependencies() -> list[str]:
+def get_runtime_dependencies(sdist: bool = False) -> list[str]:
     return [
         f"amulet-compiler-target{AMULET_COMPILER_TARGET_REQUIREMENT}",
-        f"amulet-compiler-version{AMULET_COMPILER_VERSION_REQUIREMENT}",
+        f"amulet-compiler-version{get_compiler.main(sdist)}",
         f"pybind11{PYBIND11_REQUIREMENT}",
         f"amulet-pybind11-extensions{AMULET_PYBIND11_EXTENSIONS_REQUIREMENT}",
     ]
--- src/amulet/leveldb/__init__.py
+++ src/amulet/leveldb/__init__.py
@@ -11,22 +11,9 @@ _logging.basicConfig(level=_logging.INFO, format="%(levelname)s - %(message)s")
 def _init() -> None:
     import os
     import sys
-    import ctypes
 
     if os.environ.get("AMULET_SKIP_COMPILE", None):
         return
-
-    if sys.platform == "win32":
-        lib_path = os.path.join(os.path.dirname(__file__), "leveldb.dll")
-    elif sys.platform == "darwin":
-        lib_path = os.path.join(os.path.dirname(__file__), "libleveldb.dylib")
-    elif sys.platform == "linux":
-        lib_path = os.path.join(os.path.dirname(__file__), "libleveldb.so")
-    else:
-        raise RuntimeError(f"Unsupported platform {sys.platform}")
-
-    # Load the shared library
-    ctypes.cdll.LoadLibrary(lib_path)
 
     from ._leveldb import init
 
